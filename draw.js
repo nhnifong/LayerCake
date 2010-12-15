653d9ad8baf419823410f48c3b662fdae019a551
Nathan Nifong <nhnifong@bent.(none)>
2010-12-15 01:00:55 +0000

    Seems to connect and run without errors now
    Still need to test it with two clients though. I have a feeling it wont work
    server prints no "received whatever" messages. maybe frames arent getting flushed?

--- draw.js
+++ draw.js
@@ -577,31 +577,13 @@ function dist(a,b,c,d){
     return Math.sqrt(Math.pow((c-a),2)+Math.pow((d-b),2));
 }
 
+function abs(x){
+    return Math.abs(x);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function str(s){
+    return String(s);
+}
 
 
 
